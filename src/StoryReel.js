a756d6dbb136a5cc92237cc1053ee5230d5ef6f7
blend-i <55741869+blend-i@users.noreply.github.com>
2020-09-27 21:05:58 +0200

    oppdaterte storyreel bilder

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -8,28 +8,29 @@ function StoryReel() {
         <div className="storyReel">
 
             <Story
-            image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FxkmzaE5YCwo%2Fmaxresdefault.jpg&f=1&nofb=1"
-            profileSrc="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FJReg6qKqlMs%2Fmaxresdefault.jpg&f=1&nofb=1"
-            title="Steve Fox"
+            image="https://a-static.besthdwallpaper.com/league-of-legends-lol-vayne-fantasy-kunst-tapet-1080x1920-23798_165.jpg"
+            profileSrc="https://i.pinimg.com/originals/30/0e/58/300e58c8416a68dcfcf1761501348243.jpg"
+            title="League of Legends"
             />
 
             <Story
-            image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FxkmzaE5YCwo%2Fmaxresdefault.jpg&f=1&nofb=1"
-            profileSrc="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FJReg6qKqlMs%2Fmaxresdefault.jpg&f=1&nofb=1"
-            title="Steve Fox"
+            image="https://i.pinimg.com/originals/34/4c/8c/344c8c87cf9bf9299eb021e4f8596ed4.jpg"
+            profileSrc="https://www.clipartmax.com/png/middle/226-2261836_other-discord-transparent-server-icon-images-world-of-warcraft-discord-logo.png"
+            title="World of Warcraft"
             />
 
             <Story
-            image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FxkmzaE5YCwo%2Fmaxresdefault.jpg&f=1&nofb=1"
-            profileSrc="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FJReg6qKqlMs%2Fmaxresdefault.jpg&f=1&nofb=1"
-            title="Steve Fox"
+            image="https://wallpapercave.com/wp/wp1809698.jpg"
+            profileSrc="https://upload.wikimedia.org/wikipedia/commons/5/55/Overwatch_circle_logo.svg"
+            title="Overwatch"
             />
 
             <Story
-            image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FxkmzaE5YCwo%2Fmaxresdefault.jpg&f=1&nofb=1"
-            profileSrc="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FJReg6qKqlMs%2Fmaxresdefault.jpg&f=1&nofb=1"
-            title="Steve Fox"
+            image="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/clans/33836086/1f9249103f371671071532e02e3ab39d2da49cbe.png"
+            profileSrc="https://pbs.twimg.com/media/Eij4M3LUMAA1Nf3.jpg"
+            title="Among us"
             />
+
         </div>
     )
 }
